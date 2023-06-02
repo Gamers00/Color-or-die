2d69c113eb11dc3b6ae5496e6be07872915bf432
Gamers00 <133620517+Gamers00@users.noreply.github.com>
2023-06-02 01:48:59 -0300

    Delete script error.lua

--- script error.lua
+++
@@ -1,9 +0,0 @@
-game.StarterGui:SetCore("SendNotification", {
-    Title = "Ops";
-    Text = "Sorry script not working";
-})
-wait(2)
-game.StarterGui:SetCore("SendNotification", {
-    Title = "Hey!";
-    Text = "Script Updating! please wait";
-})

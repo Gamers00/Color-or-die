91be3e76574169b39be0c44a6bc3d55df4ccc620
Gamers00 <133620517+Gamers00@users.noreply.github.com>
2023-06-02 01:46:59 -0300

    Color or die

---
+++ script error.lua
@@ -0,0 +1,9 @@
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Ops";
+    Text = "Sorry script not working";
+})
+wait(2)
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Hey!";
+    Text = "Script Updating! please wait";
+})
